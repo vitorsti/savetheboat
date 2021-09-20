9bd4f7a38fe5d60041514870ad864e0a5beb4914
DonRossini <36886780+DonRossini@users.noreply.github.com>
2021-09-20 17:07:55 -0300

    Agora vai

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnSpeed;
+    GameObject thisObt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisObt = this.gameObject;
+
+        InvokeRepeating("Spawn", 1f, 1f);
+    }
+
+    public void Spawn()
+    {
+        GameObject go = Instantiate(enemyPrefab, thisObt.transform.position, thisObt.transform.rotation, thisObt.transform);
+        go.name = enemyPrefab.name;
+    }
+}
