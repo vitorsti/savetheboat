44d34926ef274965450f99f99b508597c697b14e
DonRossini <36886780+DonRossini@users.noreply.github.com>
2021-09-23 15:57:30 -0300

    In Order in Layer
    Definido e arrumado as layers de imagens em jogo no padrão estipulado;
    Ajustado o script de paralax para colocar um pequeno offset para cada imagem que aparece em paralax;

--- Assets/Scripts/BgSpawn.cs
+++ Assets/Scripts/BgSpawn.cs
@@ -8,6 +8,8 @@ public class BgSpawn : MonoBehaviour
     public GameObject prefab;
     public Transform spawn;
 
+    public Vector3 _offSetSpawn;
+
     void Awake(){
         spawn = GameObject.Find("BgSpawner").transform;
     }
@@ -18,7 +20,10 @@ public class BgSpawn : MonoBehaviour
         {
             //Destroy(other.gameObject);
 
-            GameObject go = Instantiate(prefab, new Vector3(spawn.position.x + 0.2f,spawn.position.y + 0.03f,spawn.position.z), Quaternion.identity);
+            GameObject go = Instantiate(prefab, new Vector3(spawn.position.x + _offSetSpawn.x,
+                spawn.position.y +_offSetSpawn.y
+                ,spawn.position.z + _offSetSpawn.z), Quaternion.identity);
+
             go.transform.SetParent(spawn.gameObject.transform, true);
             go.name = objName;
 
