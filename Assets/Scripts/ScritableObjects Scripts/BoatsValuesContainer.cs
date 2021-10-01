8574e50ec269ec0f88200e045d85665326b829cd
vitorsti <vitorstigliani@gmail.com>
2021-09-30 23:38:13 -0300

    bkup - tela de compra/selecionar/comprarupgrades barco
    funcionalidades da tela de descricao do barco completa

--- Assets/Scripts/ScritableObjects Scripts/BoatsValuesContainer.cs
+++ Assets/Scripts/ScritableObjects Scripts/BoatsValuesContainer.cs
@@ -32,8 +32,31 @@ public class BoatsValuesContainer : ScriptableObject
         public bool boatSelected;
         //public bool ins;
     }
+    [Serializable]
+    public struct Upgrades
+    {
+        public string name;
+        public int id;
+        [Header("Prices of the upgrades")]
+        public float healthPrice;
+        public float speedPrice;
+        public float moneyPrice;
+        public float strengthPrice;
+        [Header("Stages of the upgrades")]
+        public int healtStage;
+        public int speedStage;
+        public int moneyStage;
+        public int strengthStage;
+        [Header("Totas stages of the upgrades")]
+        public int healtTotalStages;
+        public int speedTotalStages;
+        public int moneyTotalStages;
+        public int strengthTotalStages;
+    }
     public Boats[] boatsDatas;
     public Store[] boatsStoreDatas;
+    public Upgrades[] boatsUpgrades;
+
 
     public bool store;
 
@@ -46,16 +69,23 @@ public class BoatsValuesContainer : ScriptableObject
             {
                 boatsDatas[i].id = i;
             }
+
         }
+
         if (store)
         {
             boatsStoreDatas = new Store[boatsDatas.Length];
+            boatsUpgrades = new Upgrades[boatsDatas.Length];
 
             for (int i = 0; i < boatsDatas.Length; i++)
             {
                 boatsStoreDatas[i].name = boatsDatas[i].name;
                 boatsStoreDatas[i].image = boatsDatas[i].image;
                 boatsStoreDatas[i].id = i;
+
+                boatsUpgrades[i].name = boatsDatas[i].name;
+                boatsUpgrades[i].id = i;
+
             }
 
             store = false;
@@ -198,12 +228,12 @@ public class BoatsValuesContainer : ScriptableObject
         if (_name == null || _name == "" || _name == " ")
         {
             int i = Array.FindIndex(boatsDatas, x => x.id == _id);
-           boatsDatas[i].strength = _strength;
+            boatsDatas[i].strength = _strength;
         }
         else
         {
             int n = Array.FindIndex(boatsDatas, x => x.name == _name);
-           boatsDatas[n].strength = _strength;
+            boatsDatas[n].strength = _strength;
         }
 
     }
@@ -213,12 +243,12 @@ public class BoatsValuesContainer : ScriptableObject
         if (_name == null || _name == "" || _name == " ")
         {
             int i = Array.FindIndex(boatsDatas, x => x.id == _id);
-           boatsDatas[i].strength = _speed;
+            boatsDatas[i].strength = _speed;
         }
         else
         {
             int n = Array.FindIndex(boatsDatas, x => x.name == _name);
-           boatsDatas[n].strength = _speed;
+            boatsDatas[n].strength = _speed;
         }
 
     }
@@ -228,13 +258,261 @@ public class BoatsValuesContainer : ScriptableObject
         if (_name == null || _name == "" || _name == " ")
         {
             int i = Array.FindIndex(boatsDatas, x => x.id == _id);
-           boatsDatas[i].strength = _health;
+            boatsDatas[i].strength = _health;
         }
         else
         {
             int n = Array.FindIndex(boatsDatas, x => x.name == _name);
-           boatsDatas[n].strength = _health;
+            boatsDatas[n].strength = _health;
         }
+
+    }
+
+    //upgrades price
+    public float GetHealthUpgradePrice(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).healthPrice;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).healthPrice;
+
+    }
+
+    public void SetHealthUpgradePrice(int _id, string _name, float _value)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].healthPrice = _value;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].healthPrice = _value;
+        }
+
+    }
+
+    public float GetSpeedUpgradePrice(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).speedPrice;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).speedPrice;
+
+    }
+
+    public void SetSpeedUpgradePrice(int _id, string _name, float _value)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].speedPrice = _value;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].speedPrice = _value;
+        }
+
+    }
+
+    public float GetMoneyUpgradePrice(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).moneyPrice;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).moneyPrice;
+
+    }
+
+    public void SetMoneyUpgradePrice(int _id, string _name, float _value)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].moneyPrice = _value;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].moneyPrice = _value;
+        }
+
+    }
+
+    public float GetStrengthUpgradePrice(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).strengthPrice;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).strengthPrice;
+
+    }
+
+    public void SetStrengthUpgradePrice(int _id, string _name, float _value)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].strengthPrice = _value;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].strengthPrice = _value;
+        }
+
+    }
+
+    //end
+
+    //upgrade stage
+
+    public float GetHealthUpgradeStage(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).healtStage;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).healtStage;
+
+    }
+
+    public void SetHealthUpgradeStage(int _id, string _name, int _health)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].healtStage = _health;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].healtStage = _health;
+        }
+
+    }
+
+    public float GetSpeedUpgradeStage(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).speedStage;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).speedStage;
+
+    }
+
+    public void SetSpeedUpgradeStage(int _id, string _name, int _health)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].speedStage = _health;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].speedStage = _health;
+        }
+
+    }
+
+    public float GetMoneyUpgradeStage(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).moneyStage;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).moneyStage;
+
+    }
+
+    public void SetMoneyUpgradeStage(int _id, string _name, int _health)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].moneyStage = _health;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].moneyStage = _health;
+        }
+
+    }
+
+    public float GetStrengthUpgradeStage(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).strengthStage;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).strengthStage;
+
+    }
+
+    public void SetStrengthUpgradeStage(int _id, string _name, int _health)
+    {
+        if (_name == null || _name == "" || _name == " ")
+        {
+            int i = Array.FindIndex(boatsUpgrades, x => x.id == _id);
+            boatsUpgrades[i].strengthStage = _health;
+        }
+        else
+        {
+            int n = Array.FindIndex(boatsUpgrades, x => x.name == _name);
+            boatsUpgrades[n].strengthStage = _health;
+        }
+
+    }
+    //end
+
+    //upgrade total stage
+
+    public float GetStrengthUpgradeTotalSatege(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).strengthTotalStages;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).strengthTotalStages;
+
+    }
+
+    public float GetHealthUpgradeTotalSatege(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).healtTotalStages;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).healtTotalStages;
+
+    }
+
+    public float GetMoneyUpgradeTotalSatege(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).moneyTotalStages;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).moneyTotalStages;
+
+    }
+
+    public float GetSpeedUpgradeTotalSatege(int _id, string _name)
+    {
+
+        if (_name == null || _name == "" || _name == " ")
+            return boatsUpgrades.FirstOrDefault(x => x.id == _id).speedTotalStages;
+        else
+            return boatsUpgrades.FirstOrDefault(x => x.name == _name).speedTotalStages;
 
     }
 
---
+++ Assets/Scripts/StoreScripts/BoatInfoWindowManager.cs
@@ -0,0 +1,199 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class BoatInfoWindowManager : MonoBehaviour
+{
+    BoatsValuesContainer data;
+    public Button closeBttun;
+    public Image boatImage;
+    public TextMeshProUGUI descriptiontxt;
+    [Serializable]
+    public struct Upgrades { public string name; public TextMeshProUGUI upgradeName; public Slider upgradeStage; public Button buyBuytton; public float priceMultiplier; public float upPrice; }
+    public Upgrades[] upgrades;
+    public Button buySelectBttun;
+    public string boatName;
+    private void Awake()
+    {
+        data = Resources.Load<BoatsValuesContainer>("ScriptableObjects/BoatsData");
+        closeBttun.onClick.AddListener(Close);
+        Debug.Log(boatName);
+    }
+
+    private void Start()
+    {
+        SetThings();
+    }
+
+    public void SetThings()
+    {
+        boatImage.sprite = data.GetImage(0, boatName);
+        descriptiontxt.text = data.GetDescription(0, boatName);
+
+        SetUpgradeTab();
+
+
+        bool hasbougth = data.GetBougth(0, boatName);
+        bool selected = data.GetSelected(0, boatName);
+
+        if (!hasbougth)
+        {
+            SetBuyBoatButton();
+        }
+
+        if (hasbougth && !selected)
+        {
+            SetChooseBoatButton();
+        }
+
+        if (selected)
+        {
+            SetBoatSelectedButton();
+        }
+
+    }
+
+    public void SetUpgradeTab()
+    {
+        for (int i = 0; i < upgrades.Length; i++)
+        {
+            upgrades[i].buyBuytton.onClick.RemoveAllListeners();
+        }
+        //uipgrade order 0-health 1-speed 2-strength 3-money
+        upgrades[0].upPrice = data.GetHealthUpgradePrice(0, boatName);
+        upgrades[1].upPrice = data.GetSpeedUpgradePrice(0, boatName);
+        upgrades[2].upPrice = data.GetStrengthUpgradePrice(0, boatName);
+        upgrades[3].upPrice = data.GetMoneyUpgradePrice(0, boatName);
+
+        if (data.GetHealthUpgradeStage(0, boatName) != data.GetHealthUpgradeTotalSatege(0, boatName))
+            upgrades[0].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = upgrades[0].upPrice.ToString();
+        else
+            upgrades[0].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = "MAX";
+
+        if (data.GetSpeedUpgradeStage(0, boatName) != data.GetSpeedUpgradeTotalSatege(0, boatName))
+            upgrades[1].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = upgrades[1].upPrice.ToString();
+        else
+            upgrades[1].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = "MAX";
+
+        if (data.GetStrengthUpgradeStage(0, boatName) != data.GetStrengthUpgradeTotalSatege(0, boatName))
+            upgrades[2].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = upgrades[2].upPrice.ToString();
+        else
+            upgrades[2].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = "MAX";
+
+        if (data.GetMoneyUpgradeStage(0, boatName) != data.GetMoneyUpgradeTotalSatege(0, boatName))
+            upgrades[3].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = upgrades[3].upPrice.ToString();
+        else
+            upgrades[3].buyBuytton.GetComponentInChildren<TextMeshProUGUI>().text = "MAX";
+
+        upgrades[0].upgradeStage.maxValue = data.GetHealthUpgradeTotalSatege(0, boatName);
+        upgrades[1].upgradeStage.maxValue = data.GetSpeedUpgradeTotalSatege(0, boatName);
+        upgrades[2].upgradeStage.maxValue = data.GetStrengthUpgradeTotalSatege(0, boatName);
+        upgrades[3].upgradeStage.maxValue = data.GetMoneyUpgradeTotalSatege(0, boatName);
+
+        upgrades[0].upgradeStage.value = data.GetHealthUpgradeStage(0, boatName);
+        upgrades[1].upgradeStage.value = data.GetSpeedUpgradeStage(0, boatName);
+        upgrades[2].upgradeStage.value = data.GetStrengthUpgradeStage(0, boatName);
+        upgrades[3].upgradeStage.value = data.GetMoneyUpgradeStage(0, boatName);
+
+        for (int i = 0; i < upgrades.Length; i++)
+        {
+            upgrades[i].upgradeName.text = upgrades[i].name + ": lvl " + upgrades[i].upgradeStage.value.ToString();
+            int index = i;
+            upgrades[i].buyBuytton.onClick.AddListener(delegate { BuyUpgrade(index); });
+        }
+    }
+
+    public void SetBuyBoatButton()
+    {
+        buySelectBttun.onClick.RemoveAllListeners();
+        buySelectBttun.onClick.AddListener(Buy);
+        buySelectBttun.GetComponentInChildren<TextMeshProUGUI>().text = "Price: " + data.GetPrice(0, boatName);
+    }
+
+    public void SetChooseBoatButton()
+    {
+        buySelectBttun.onClick.RemoveAllListeners();
+        buySelectBttun.onClick.AddListener(Select);
+        buySelectBttun.GetComponentInChildren<TextMeshProUGUI>().text = "Use Boat";
+    }
+
+    public void SetBoatSelectedButton()
+    {
+        buySelectBttun.onClick.RemoveAllListeners();
+        buySelectBttun.GetComponentInChildren<TextMeshProUGUI>().text = "Boat Selected";
+        buySelectBttun.interactable = false;
+
+    }
+
+    public void Buy()
+    {
+        data.SetHasbougth(0, boatName, true);
+        Debug.Log("boat bougth");
+        SetChooseBoatButton();
+    }
+
+    public void Select()
+    {
+        for (int i = 0; i < data.boatsDatas.Length; i++)
+        {
+            if (data.GetSelected(i, null))
+                data.SetSelected(i, null, false);
+        }
+
+        data.SetSelected(0, boatName, true);
+        Debug.Log("boat selected");
+        SetBoatSelectedButton();
+        BoatsContentPageManager pageManager = FindObjectOfType<BoatsContentPageManager>();
+        pageManager.Spawn(pageManager.page);
+    }
+
+    public void BuyUpgrade(int index)
+    {
+        if (upgrades[index].upgradeStage.value == upgrades[index].upgradeStage.maxValue)
+            return;
+        float multi = upgrades[index].priceMultiplier;
+        float newPrice = 0;
+        upgrades[index].upgradeStage.value++;
+        switch (index)
+        {
+            case 0:
+                newPrice = data.GetHealthUpgradePrice(0, boatName) * multi;
+                data.SetHealthUpgradePrice(0, boatName, (int)newPrice);
+                data.SetHealthUpgradeStage(0, boatName, (int)upgrades[index].upgradeStage.value);
+                Debug.Log(index);
+                break;
+            case 1:
+                newPrice = data.GetSpeedUpgradePrice(0, boatName) * multi;
+                data.SetSpeedUpgradePrice(0, boatName, (int)newPrice);
+                data.SetSpeedUpgradeStage(0, boatName, (int)upgrades[index].upgradeStage.value);
+                Debug.Log(index);
+                Debug.Log(index);
+                break;
+            case 2:
+                newPrice = data.GetStrengthUpgradePrice(0, boatName) * multi;
+                data.SetStrengthUpgradePrice(0, boatName, (int)newPrice);
+                data.SetStrengthUpgradeStage(0, boatName, (int)upgrades[index].upgradeStage.value);
+                Debug.Log(index);
+                Debug.Log(index);
+                break;
+            case 3:
+                newPrice = data.GetMoneyUpgradePrice(0, boatName) * multi;
+                data.SetMoneyUpgradePrice(0, boatName, (int)newPrice);
+                data.SetMoneyUpgradeStage(0, boatName, (int)upgrades[index].upgradeStage.value);
+                Debug.Log(index);
+                Debug.Log(index);
+                break;
+        }
+
+        SetUpgradeTab();
+    }
+
+    public void Close()
+    {
+
+        Destroy(this.gameObject);
+    }
+}
